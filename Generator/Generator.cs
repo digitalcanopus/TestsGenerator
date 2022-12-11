617c7dc8a0da1fc8684f090db24cc80edebe3fea
digitalcanopus <rocky-cat@mail.ru>
2022-12-11 21:04:36 +0300

    generator class added

---
+++ Generator/Generator.cs
@@ -0,0 +1,23 @@
+﻿namespace Generator
+{
+    public class Generator
+    {
+        public class ClassInfo
+        {
+            public string ClassName { get; }
+            public string TestsFile { get; }
+
+            public ClassInfo(string className, string testsFile)
+            {
+                ClassName = className;
+                TestsFile = testsFile;
+            }
+        }
+
+        private class ClassData
+        {
+            public string ClassName { get; }
+
+        }
+    }
+}
